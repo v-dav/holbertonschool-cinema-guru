954823ce838d0addb0dc4daadbce8e510a784cd9
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-11 13:55:52 +0200

    import input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser} from '@fortawesome/free-solid-svg-icons';
+import Input from "./components/general/Input";
+
+library.add(faUser)
+
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	const [inputValue, setInputValue] = useState("");
+	return (
+		<div className="App">
+			<Input
+				label={"Username:"}
+				type={"text"}
+				className={"general-input"}
+				value={inputValue}
+				setValue={setInputValue}
+				icon={"fa-solid fa-user"}
+				inputAttributes={{ id: "general-input", name: "general-input"}}
+			></Input>
+		</div>
+	);
 }
 
 export default App;
