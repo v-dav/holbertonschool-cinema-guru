bf0f1c6a4c3cb7aa772170f142062c483be1f872
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-11 14:43:28 +0200

    add the select input component to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser} from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import Input from "./components/general/Input";
+import SelectInput from "./components/general/SelectInput";
 
 library.add(faUser)
 
 
 function App() {
+	const selectOptions = [
+		"Default",
+		"Latest",
+		"Oldest",
+		"Highest Rated",
+		"Lowest Rated"
+	];
 	const [inputValue, setInputValue] = useState("");
+	const [selectInputValue, setSelectInputValue] = useState(selectOptions[0]);
+
 	return (
 		<div className="App">
 			<Input
@@ -17,8 +27,17 @@ function App() {
 				value={inputValue}
 				setValue={setInputValue}
 				icon={"fa-solid fa-user"}
-				inputAttributes={{ id: "general-input", name: "general-input"}}
+				inputAttributes={{ id: "general-input", name: "general-input" }}
 			></Input>
+			<SelectInput
+				label={"Sort:"}
+				options={selectOptions}
+				className={"select-input"}
+				value={selectInputValue}
+				setValue={setSelectInputValue}
+			>
+
+			</SelectInput>
 		</div>
 	);
 }
--- src/components/general/SelectInput.jsx
+++ src/components/general/SelectInput.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import './general.css';
 
 
-const SelectInput = ({label, options, className, value, setValue}) => {
-  return (
-    <div>SelectInput</div>
-  )
+const SelectInput = ({ label, options, className, value, setValue }) => {
+	const handleSelect = (e) => {
+		setValue(e.target.value);
+	}
+
+	return (
+		<div>
+			<label htmlFor="">{label}</label>
+			<select
+				name="select-input"
+				id="select-input"
+				className={className}
+				value={value}
+				onChange={handleSelect}>
+				{options.map(option => <option value={option}>{ option }</option>)}
+			</select>
+		</div>
+	)
 }
 
 export default SelectInput
