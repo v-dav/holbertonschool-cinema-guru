2fe59ea5c38d97facadb18f37f6426d5f5e3a936
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-13 11:38:08 +0200

    create register component

--- src/routes/auth/Register.jsx
+++ src/routes/auth/Register.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import './auth.css'
+import Input from '../../components/general/Input'
+import Button from '../../components/general/Button'
 
 
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
+const Register = ({ username, password, setUsername, setPassword }) => {
+	return (
+		<>
+			<form action="" className='login-form'>
+				<h3>Create a new account</h3>
+				<Input
+					label={"Username:"}
+					type={"text"}
+					className={"general-input"}
+					value={username}
+					setValue={setUsername}
+					icon={"fa-solid fa-user"}
+					inputAttributes={{ id: "general-input", name: "general-input" }}
+				></Input>
+				<Input
+					label={"Password:"}
+					type={"password"}
+					className={"general-input"}
+					value={password}
+					setValue={setPassword}
+					icon={"fa-solid fa-key"}
+					inputAttributes={{ id: "general-input", name: "general-input" }}>
+				</Input>
+				<Button
+					label={"Sign Up"}
+					className={"button signin-submit-button"}
+					icon={"fa-solid fa-plus"}
+				></Button>
+			</form>
+
+		</>
+	)
 }
 
 export default Register
