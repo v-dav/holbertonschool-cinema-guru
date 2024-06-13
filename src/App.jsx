7601a1092cf101a8a7303600cffc4125eb5c30c5
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-13 08:55:47 +0200

    initialize app component with logged in or not state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSearch, faSpinner, faUser } from '@fortawesome/free-solid-svg-icons';
 import Input from "./components/general/Input";
 import SelectInput from "./components/general/SelectInput";
 import Button from "./components/general/Button";
 import SearchBar from "./components/general/SearchBar";
+import axios from "axios";
+import './App.css';
 
 library.add(faUser, faSpinner, faSearch)
 
@@ -17,48 +19,75 @@ function App() {
 		"Highest Rated",
 		"Lowest Rated"
 	];
+
 	const [inputValue, setInputValue] = useState("");
 	const [selectInputValue, setSelectInputValue] = useState(selectOptions[0]);
 	const [title, setTitle] = useState("");
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [userUsername, setUserUsername] = useState("");
+
+	useEffect(() => {
+		const accessToken = localStorage.getItem('accessToken');
+		if (accessToken) {
+			axios.post('/api/auth', {}, {
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': `Bearer ${accessToken}`
+				}
+			})
+				.then(response => {
+					setIsLoggedIn(true);
+					setUserUsername(response.username);
+				})
+				.catch(error => {
+					alert(error);
+				})
+		}
+	}, []);
+
 
 	const handleButtonClick = () => {
 		alert("Button Clicked");
 	}
 
-	return (
-		<div className="App">
-			<Input
-				label={"Username:"}
-				type={"text"}
-				className={"general-input"}
-				value={inputValue}
-				setValue={setInputValue}
-				icon={"fa-solid fa-user"}
-				inputAttributes={{ id: "general-input", name: "general-input" }}
-			></Input>
-			<SelectInput
-				label={"Sort:"}
-				options={selectOptions}
-				className={"select-input"}
-				value={selectInputValue}
-				setValue={setSelectInputValue}
-			>
+	if (isLoggedIn) {
+		return (
+			<div className="App">
+				<Input
+					label={"Username:"}
+					type={"text"}
+					className={"general-input"}
+					value={inputValue}
+					setValue={setInputValue}
+					icon={"fa-solid fa-user"}
+					inputAttributes={{ id: "general-input", name: "general-input" }}
+				></Input>
+				<SelectInput
+					label={"Sort:"}
+					options={selectOptions}
+					className={"select-input"}
+					value={selectInputValue}
+					setValue={setSelectInputValue}
+				>
 
-			</SelectInput>
-			<Button
-				label={"Load More..."}
-				className={"button"}
-				onClick={handleButtonClick}
+				</SelectInput>
+				<Button
+					label={"Load More..."}
+					className={"button"}
+					onClick={handleButtonClick}
 				// icon={"fa-solid fa-spinner"}
-			></Button>
-			
-			<SearchBar 
-				title={title}
-				setTitle={setTitle}
-				icon={faSearch}
-			></SearchBar>
-		</div>
-	);
+				></Button>
+
+				<SearchBar
+					title={title}
+					setTitle={setTitle}
+					icon={faSearch}
+				></SearchBar>
+			</div>
+		);
+	} else {
+		return <h1>Authentication component</h1>
+	}
 }
 
 export default App;
