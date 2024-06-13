298623175e0a0f20153c0825e4b512000a7013f4
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-13 11:01:33 +0200

    integrate and style authentication form

--- src/routes/auth/Authentication.jsx
+++ src/routes/auth/Authentication.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './auth.css'
 import Button from '../../components/general/Button';
+import Login from './Login';
 
 const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
 	const [_switch, setSwitch] = useState(true);
@@ -10,18 +11,29 @@ const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
 
 
 	return (
-		<form className='auth-form'>
-			<Button
-				label={"Sign In"}
-				className={`button signin-button ${activeButton === 'signIn' ? 'active' : ''}`}
-				onClick={(e) => { e.preventDefault(); setSwitch(true); setActiveButton('signIn') }}>
-			</Button>
-			<Button
-				label={"Sign Up"}
-				className={`button signup-button ${activeButton === 'signUp' ? 'active' : ''}`}
-				onClick={(e) => { e.preventDefault(); setSwitch(false); setActiveButton('signUp') }}>
-			</Button>
-		</form>
+		<>
+			<div className='auth-container'>
+				<form className='auth-form'>
+					<Button
+						label={"Sign In"}
+						className={`button signin-button ${activeButton === 'signIn' ? 'active' : ''}`}
+						onClick={(e) => { e.preventDefault(); setSwitch(true); setActiveButton('signIn') }}>
+					</Button>
+					<Button
+						label={"Sign Up"}
+						className={`button signup-button ${activeButton === 'signUp' ? 'active' : ''}`}
+						onClick={(e) => { e.preventDefault(); setSwitch(false); setActiveButton('signUp') }}>
+					</Button>
+				</form>
+				<Login
+					username={username}
+					password={password}
+					setUsername={setUsername}
+					setPassword={setPassword}
+				></Login>
+			</div>
+
+		</>
 	)
 }
 
