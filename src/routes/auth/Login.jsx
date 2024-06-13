c81650b2b7822183b785e2d1c613f26f8a411993
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-13 11:01:18 +0200

    wrtie login component

--- src/routes/auth/Login.jsx
+++ src/routes/auth/Login.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import './auth.css'
+import Input from '../../components/general/Input'
+import Button from '../../components/general/Button'
 
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+const Login = ({ username, password, setUsername, setPassword }) => {
+	return (
+		<>
+			<form action="" className='login-form'>
+				<h3>Sign in with your account</h3>
+				<Input
+					label={"Username:"}
+					type={"text"}
+					className={"general-input"}
+					value={username}
+					setValue={setUsername}
+					icon={"fa-solid fa-user"}
+					inputAttributes={{ id: "general-input", name: "general-input" }}
+				></Input>
+				<Input
+					label={"Password:"}
+					type={"password"}
+					className={"general-input"}
+					value={password}
+					setValue={setPassword}
+					icon={"fa-solid fa-key"}
+					inputAttributes={{ id: "general-input", name: "general-input" }}>
+				</Input>
+				<Button
+					label={"Sign In"}
+					className={"button signin-submit-button"}
+					icon={"fa-solid fa-key"}
+				></Button>
+			</form>
+
+		</>
+	)
 }
 
 export default Login
