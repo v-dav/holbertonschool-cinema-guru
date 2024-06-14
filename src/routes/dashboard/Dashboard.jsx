c4e2b1a4539a1f6826aabf991f74602111174eb4
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-14 12:53:24 +0200

    import side bar in the dashboard

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './dashboard.css'
 import Header from '../../components/navigation/Header'
+import SideBar from '../../components/navigation/SideBar'
 
 const Dashboard = ({ userUsername, setIsLoggedIn }) => {
 	return (
 		<div className='dashboard'>
 			<Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn}></Header>
+			<SideBar></SideBar>
 		</div>
 	)
 }
